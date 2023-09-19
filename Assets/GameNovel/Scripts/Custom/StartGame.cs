828ad946779c04a21850fdc804e435d22af9aa90
Cordilyidae <one.puchman.1000@gmail.com>
2023-09-19 22:18:33 +0300

    Add custom command

---
+++ Assets/GameNovel/Scripts/Custom/StartGame.cs
@@ -0,0 +1,26 @@
+using Naninovel;
+using UnityEngine;
+
+namespace GameNovel
+{
+    [CommandAlias("Hello")]
+    public class StartGame : Command
+    {
+        public StringParameter Name;
+
+        public override UniTask ExecuteAsync(AsyncToken asyncToken = default)
+        {
+            if (Assigned(Name))
+            {
+                Debug.Log($"Fuck bull, {Name}!");
+            }
+            else
+            {
+                Debug.Log("Fuck this bull!");
+            }
+
+            return UniTask.CompletedTask;
+        }
+    }
+}
+
